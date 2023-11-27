c44ca0c3040da41fd1ecf5652de549176e28a07e
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2023-11-26 23:04:38 -0300

    Ajusted Snail and added method for kill snail

--- Assets/Pixel Adventure 1/Assets/Scripts/Snail.cs
+++ Assets/Pixel Adventure 1/Assets/Scripts/Snail.cs
@@ -1,29 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Snail : MonoBehaviour {
-    public float speed = 1f;  
-    private int direction = 1;  
+    private Animator animator;
 
-    private void Update() {
-        Move();
-        CheckCollision();
+    public float speed;
+    public float moveTime;
+    public Transform headPoint;
+
+    private bool directionRight = false;
+    private float timer;
+
+    void Start() {
+        animator = GetComponent<Animator>();
     }
 
-    private void Move() {
-        Vector2 movement = new Vector2(speed * direction * Time.deltaTime, 0);
-        transform.Translate(movement);
-    }
+    void Update() {
+        if(directionRight) {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            
+        } else {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
 
-    private void CheckCollision() {
-        Vector2 startPoint = new Vector2(transform.position.x + direction * 0.5f, transform.position.y);
-        RaycastHit2D hit = Physics2D.Raycast(startPoint, new Vector2(direction, 0), 0.1f);
-
-        if (hit.collider != null) {
-            direction *= -1;
-
-            Vector3 scale = transform.localScale;
-            scale.x *= -1;
-            transform.localScale = scale;
+        timer += Time.deltaTime;
+        if(timer >= moveTime) {
+            Flip();
+            timer = 0f;
         }
     }
+
+        private void Flip() {
+        directionRight = !directionRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.CompareTag("Player")) {
+            float height = collision.contacts[0].point.y - headPoint.position.y;
+           
+            if(height > 0) {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 8, ForceMode2D.Impulse);
+                speed = 0;
+                animator.SetTrigger("die");
+                Destroy(gameObject, 0.33f);
+            } else {
+                collision.collider.GetComponent<Player>().Hit();
+            }
+        }
+    }
+
 }
